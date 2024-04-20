4a2b499cfa0ae549d481ecf7f64f54434d3dfe21
Alex <ango911s@gmail.com>
2024-04-20 15:48:17 -0700

    fixed merge

--- lahacks24/src/pages/Home.js
+++ lahacks24/src/pages/Home.js
@@ -1,12 +1,14 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import { ReactTyped } from "react-typed";
 
 const Home = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return (
-    <div className="absolute">
+  return (<div>
+    <div className='bg-slate-300'>
+    <div className='absolute'>
       <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+        <ul class="font-bold flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
           <li>
             <a
               href="/"
@@ -40,13 +42,23 @@ const Home = () => {
             </a>
           </li>
         </ul>
-        <button
-          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          onClick={() => loginWithRedirect()}
-        >
-          Log In
-        </button>
       </div>
+    </div>
+    <div className='pt-3 pr-4 flex justify-end'>
+      <button
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded "
+        onClick={() => loginWithRedirect()}
+      >
+        Log In
+      </button>
+    </div>
+    <div className='text-center flex justify-center items-center h-screen'>
+      <h1 className='font-bold text-8xl'>
+        Welcome to<br></br> 
+        <ReactTyped strings={["Recipes Reimagined"]} className='text-blue-500' typeSpeed={150} loop backSpeed={150}/>
+      </h1>
+    </div>
+    </div>
     </div>
   );
 };
