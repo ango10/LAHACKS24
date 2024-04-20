b4023f6789eb197399502277f7b20cf96b5b4db7
Riley Wong <rileysw@uci.edu>
2024-04-20 10:41:01 -0700

    resolve merge conflicts with camera and auth

--- lahacks24/src/App.js
+++ lahacks24/src/App.js
@@ -1,6 +1,7 @@
-import './App.css';
+import "./App.css";
 
 import ReactDOM from "react-dom/client";
+import { Auth0Provider } from "@auth0/auth0-react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Camera from "./pages/Camera";
@@ -10,20 +11,27 @@ import NoPage from "./pages/NoPage";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
+    <Auth0Provider
+      domain="dev-on1vy228eirwa2oo.us.auth0.com"
+      clientId="ctkQfWr55tdgLX3VkpZ2ezbg9IELBFLE"
+      authorizationParams={{
+        redirect_uri: "http://localhost:3000/Camera",
+      }}
+    >
+      <BrowserRouter>
+        <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Camera" element={<Camera />} />
           <Route path="/View" element={<View />} />
           <Route path="/Recipes" element={<Recipes />} />
           <Route path="*" element={<NoPage />} />
-      </Routes>
-    </BrowserRouter>
+        </Routes>
+      </BrowserRouter>
+    </Auth0Provider>
   );
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
 
 export default App;
-
--- lahacks24/src/pages/Home.js
+++ lahacks24/src/pages/Home.js
@@ -1,22 +1,54 @@
-const Home = () => {
-    return <div className='absolute'>
-        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                <li>
-                <a href="/" class="block py-2 px-3 text-gray-900 rounded md:border-0 md:p-0 dark:text-white :bg-transparent">Home</a>
-                </li>
-                <li>
-                <a href="/Camera" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Enter Ingredients</a>
-                </li>
-                <li>
-                <a href="/View" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">View Ingredients</a>
-                </li>
-                <li>
-                <a href="/Recipes" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Recipes</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-    };
+import { useAuth0 } from "@auth0/auth0-react";
 
-  export default Home;
+const Home = () => {
+  const { loginWithRedirect } = useAuth0();
+
+  return (
+    <div className="absolute">
+      <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <li>
+            <a
+              href="/"
+              class="block py-2 px-3 text-gray-900 rounded md:border-0 md:p-0 dark:text-white :bg-transparent"
+            >
+              Home
+            </a>
+          </li>
+          <li>
+            <a
+              href="/Camera"
+              class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+            >
+              Enter Ingredients
+            </a>
+          </li>
+          <li>
+            <a
+              href="/View"
+              class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+            >
+              View Ingredients
+            </a>
+          </li>
+          <li>
+            <a
+              href="/Recipes"
+              class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+            >
+              Recipes
+            </a>
+          </li>
+        </ul>
+        <button
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={() => loginWithRedirect()}
+        >
+          Log In
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
